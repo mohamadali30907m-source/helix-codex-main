ead330eba4db27f781c2342793745c72bd86074e
Mohamad Ali Ahmed <mohamadali30907m@gmail.com>
2026-09-01 07:20:55 +0300

    replace spline DNA with svg lab

--- nur-tech-academy/src/components/UI/DNAViewer.jsx
+++ nur-tech-academy/src/components/UI/DNAViewer.jsx
@@ -1,41 +1,14 @@
-import { useRef } from "react";
-import Spline from '@splinetool/react-spline';
+import heroImg from "../export-2026-09-01 035523.svg";
 import "./DNAViewer.css";
 
 function DNAViewer({ status = "ACTIVE" }) {
-  const splineRef = useRef(null);
-
-  const onLoad = (spline) => {
-    
-    const possibleNames = ['DNA', 'Helix', 'Group', 'Object', 'Scene'];
-    let target = null;
-    
-    for (const name of possibleNames) {
-      try {
-        target = spline.findObjectByName(name);
-        if (target) break;
-      } catch (e) {
-      
-      }
-    }
-
-    if (target && target.rotation) {
-      const animate = () => {
-        target.rotation.y += 0.008;
-        requestAnimationFrame(animate);
-      };
-      animate();
-    }
-  };
-
   return (
     <div className="dna-wrapper">
       <div className="dna-inner">
-        <Spline 
-          ref={splineRef}
-          scene="https://prod.spline.design/HhPetSmCrYYuQPgD/scene.splinecode"
-          onLoad={onLoad}
-          style={{ width: '100%', height: '100%' }}
+        <img 
+          src={heroImg} 
+          alt="Helix Codex Science Illustration" 
+          className="hero-custom-img" 
         />
       </div>
       <div className="dna-hud">CORE_STATUS // {status}</div>
